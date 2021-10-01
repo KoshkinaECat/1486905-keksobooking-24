45c932ebfa3cc842d093520c44fe7d3a14dbabd6
Ekaterina Koshkina <johnya_ya@mail.ru>
2021-10-01 22:18:59 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -6,20 +6,16 @@ function getRandomIntInclusive(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  else {
-    return ('Задайте корректный диапозон!');
-  }
+    return null;
 }
 getRandomIntInclusive(1,1);
 
 //вторая функция
-function getRandomArbitrary(min, max) {
+function getRandomArbitrary(min, max, num) {
   if (min < max && max >= 0 && min >= 0){
-    return Math.random() * (max - min) + min;
+    return Math.random().toFixed(num) * (max - min) + min;
   }
-  else{
-    return('Задайте другие числа!');
-  }
+    return NaN;
 }
 
 getRandomArbitrary(1,2);
