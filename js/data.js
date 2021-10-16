7d0914b078aa53e93b43e6107d11709c68cafb00
Ekaterina Koshkina <johnya_ya@mail.ru>
2021-10-16 20:01:05 +0300

    version 2

---
+++ js/data.js
@@ -0,0 +1,53 @@
+
+
+
+// TYPE
+
+const TYPE = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel'
+];
+
+//chekin
+const CHEKIN = [
+  '12:00',
+  '13:00',
+  '14:00'
+]
+
+//chekout
+const CHECKOUT = [
+  '12:00',
+  '13:00',
+  '14:00'
+]
+
+
+//description
+
+const DESCRIPTION = [
+  'два стола, два стула, машина времени',
+  'роскошная уютная обстановка в восточном стиле',
+  'двухярусная кровать, джакузи',
+  'комната для четверых гостей со всеми удобствами',
+  'кальян, икс-бокс и мини-бар',
+  'бильярд+дартс',
+  'детская игровая комната+бассейн с шариками',
+  'выручай-комната',
+  'кровать размера кинг-сайз',
+  'домашний кинотеатр'
+]
+
+//photos
+
+let PHOTOS = ['https: //assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https: //assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https: //assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+]
+
+// export {
+//   AUTHOR,TITLE, TYPE, CHEKIN, CHECKOUT, FEATURES, DESCRIPTION, PHOTOS
+// }
